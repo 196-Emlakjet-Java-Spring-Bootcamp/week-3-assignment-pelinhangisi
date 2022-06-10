689895fc64990bc5df9bcc48cdb102c3796c1d4d
pelinhangisi <pelinhangisi@gmail.com>
2022-06-10 15:04:49 +0300

    Publisher dosyaları oluşturuldu.

--- rabbitMQPublisher/src/main/java/com/pelinhangisi/rabbitmqpublisher/RabbitMqPublisherApplication.java
+++ rabbitMQPublisher/src/main/java/com/pelinhangisi/rabbitmqpublisher/RabbitMqPublisherApplication.java
@@ -1,9 +1,11 @@
 package com.pelinhangisi.rabbitmqpublisher;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class RabbitMqPublisherApplication {
 
     public static void main(String[] args) {
